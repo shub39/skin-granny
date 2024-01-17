665b7f5022ff94b0dbf047c300be07cca4e6646f
Shub <cptnshubham39@gmail.com>
2024-01-17 15:10:02 +0530

    working dumb bot

--- responses.py
+++ responses.py
@@ -1,13 +1,20 @@
-import random
+from random import choice, randint
 
-def handle_responses(message) -> str:
-    p_message = message.lower()
 
-    if p_message == 'hello':
-        return 'Hello!'
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
 
-    if p_message == 'roll':
-        return str(random.randint(1,6))    
-
-    if p_message == '!help':
-        return '`this is a help message to be modified`'   
+    if lowered == '':
+        return 'Well, you\'re awfully silent...'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Good, thanks!'
+    elif 'bye' in lowered:
+        return 'See you!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {randint(1, 6)}'
+    else:
+        return choice(['I do not understand...',
+                       'What are you talking about?',
+                       'Do you mind rephrasing that?'])
